c035f73c383632ccde9d3bc7eba7a381c3dad929
Gustavo Carvalho <gcbrito@hotmail.com>
2024-12-03 20:40:56 -0300

    Adicionada a função "adicionar"

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-/*
-teste
-*/
+let tarefas = [];
+function adicionar () {
+    let entrada = document.getElementById('txtn').value;
+    if (entrada.trim() === '') {
+        alert('Por favor, digite uma tarefa!');
+        return;
+    }
+    tarefas.push(entrada);
+}
+
+
+
+let input = document.createElement('txtn').value;
+tarefas.push(input)
+updateList();
+let listElement = document.getElementById('tab');
+listElement.innerHTML = '';
+tarefas.forEach(function(item) {
+    let listItem = document.createElement('li');
+    listItem.textContent = item;
+    listElement.appendChild(listItem);
+})
